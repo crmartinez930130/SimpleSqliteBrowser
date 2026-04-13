bfb6be9ea12acf40bfc4b8c20f216391098b6e72
Carlos Rodriguez <crmartinez930130@gmail.com>
2026-04-13 15:29:19 -0300

    Add support extension .sqlite

--- src/main/kotlin/com/github/gitofleonardo/simplesqlitebrowser/provider/SqliteEditorProvider.kt
+++ src/main/kotlin/com/github/gitofleonardo/simplesqlitebrowser/provider/SqliteEditorProvider.kt
@@ -1,6 +1,7 @@
 package com.github.gitofleonardo.simplesqlitebrowser.provider
 
 import com.github.gitofleonardo.simplesqlitebrowser.EXTENSION
+import com.github.gitofleonardo.simplesqlitebrowser.EXTENSION_SQLITE
 import com.github.gitofleonardo.simplesqlitebrowser.SQLITE_LANGUAGE
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorPolicy
@@ -12,7 +13,7 @@ import com.intellij.openapi.vfs.VirtualFile
 
 class SqliteEditorProvider : FileEditorProvider, DumbAware {
     override fun accept(project: Project, file: VirtualFile): Boolean {
-        return file.extension == EXTENSION
+        return file.extension == EXTENSION || file.extension == EXTENSION_SQLITE
     }
 
     override fun createEditor(project: Project, file: VirtualFile): FileEditor {
